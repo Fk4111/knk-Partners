e8641f08ccbf265f756472a6ecad3830433da142
Fk4111 <khanfaiyaz359@gmail.com>
2026-05-24 17:13:10 +0530

    change cors policies

--- knk-backend/server.js
+++ knk-backend/server.js
@@ -19,7 +19,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://knk-panel.vercel.app"
+      "https://knk-partners.vercel.app"
     ],
     credentials: true,
   })
